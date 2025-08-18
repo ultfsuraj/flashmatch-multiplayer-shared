62d1c1f9a193cb027a57c2556a96b165603e7f3d
R B suraj <rbsuraj432@gmail.com>
2025-08-18 18:10:28 +0530

    added .js file tsc shared.ts, for node to use constants at runtime

---
+++ shared.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Games = void 0;
+exports.Games = {
+    chess: {
+        name: 'chess',
+        minPlayers: 2,
+        maxPlayers: 2,
+    },
+    colorWars: {
+        name: 'colorWars',
+        minPlayers: 2,
+        maxPlayers: 2,
+    },
+    ludo: {
+        name: 'ludo',
+        minPlayers: 2,
+        maxPlayers: 4,
+    },
+};

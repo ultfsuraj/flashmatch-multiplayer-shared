b37ab947910aba1369cc0ccbbc8cacdb132eef7d
R B suraj <rbsuraj432@gmail.com>
2025-08-18 16:08:48 +0530

    types for Games, joining room , and player info

--- shared.d.ts
+++ shared.d.ts
@@ -1,1 +1,35 @@
-export type Ping = { type: 'ping' };
+export type Games = {
+  chess: {
+    name: 'chess';
+    minPlayers: 2;
+    maxPlayers: 2;
+  };
+  colorWars: {
+    name: 'colorWars';
+    minPlayers: 2;
+    maxPlayers: 2;
+  };
+  ludo: {
+    name: 'ludo';
+    minPlayers: 2;
+    maxPlayers: 4;
+  };
+};
+
+export type Events = {
+  playerJoined: {
+    name: 'playerJoined';
+    payload: {
+      number: number;
+      playerName: string;
+    };
+  };
+  joinRoom: {
+    name: 'joinRoom';
+    payload: {
+      gameName: string;
+      roomid: string;
+      playerName: string;
+    };
+  };
+};
